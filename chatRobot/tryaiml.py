d54cfa43379dc2f8e67e01977e9e21c3064a54b6
yang <547941595@qq.com>
2018-04-09 21:15:49 +0800

    musicSearch.aiml初步完成，歌名，歌手，音乐标签搜索对话

--- chatRobot/tryaiml.py
+++ chatRobot/tryaiml.py
@@ -2,18 +2,20 @@ import aiml
 import os
 
 #切换路径到chatRobot
-path = './'
-os.chdir(path)
+mybot_path = './'
+aiml_path = './musicSearch.aiml'
+os.chdir(mybot_path)
 
 # Create the kernel and learn AIML files
 mybot = aiml.Kernel()
+mybot.learn(aiml_path)
 
-#brain加速
-if os.path.isfile("mybot_brain.brn"):
-    mybot.bootstrap(brainFile="mybot_brain.brn")
-else:
-    mybot.bootstrap(learnFiles="musicSearch.aiml", commands="load aiml b")
-    mybot.saveBrain("mybot_brain.brn")
+# #brain加速。   如果删除文件mybot_brain.brn重新加载aiml文件
+# if os.path.isfile("mybot_brain.brn"):
+#     mybot.bootstrap(brainFile="mybot_brain.brn")
+# else:
+#     mybot.bootstrap(learnFiles="musicSearch.aiml", commands="load aiml b")
+#     mybot.saveBrain("mybot_brain.brn")
 
 # Press CTRL-C to break this loop
 while True:
